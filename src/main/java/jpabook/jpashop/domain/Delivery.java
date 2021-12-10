7aad28bb17800cce90da2bf10bff47708fb5b831
nect2r <nect2r@kakao.com>
2021-12-10 13:34:23 +0900

    다양한 연관관계 매핑 실습

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,22 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Delivery {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String city;
+    private String street;
+    private String zipcode;
+    private DeliveryStatus status;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+}
